fe66ed81a9144883e3a1a4f41e6e263f856f39c3
pdias-felipe <pdias.felipe@gmail.com>
2022-11-24 23:13:40 -0300

    routes ok

---
+++ src/routes/Home.jsx
@@ -0,0 +1,10 @@
+//digita RAFCE e cria o componente
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
---
+++ src/routes/NewPost.jsx
@@ -0,0 +1,10 @@
+//digita RAFCE e cria o componente
+import React from 'react';
+
+const NewPost = () => {
+  return (
+    <div>NewPost</div>
+  )
+}
+
+export default NewPost;
