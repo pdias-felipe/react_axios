ea5afa71fd34360affac36e9dc49e6dcd83c36c5
pdias-felipe <pdias.felipe@gmail.com>
2022-11-25 14:04:04 -0300

    requisição da API na home.jsx ok

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,31 @@
+import axios from 'axios';
+
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 //digita RAFCE e cria o componente
-import React from 'react';
+
 
 const Home = () => {
+
+  const[posts, setPosts] = useState([])
+
+  const getPosts = async() => { /*responsavel por pegar os dados da API -- tipo fetch*/
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const data = response.data;
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+  useEffect
+
   return (
     <div>Home</div>
   )
