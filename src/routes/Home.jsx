fa37b80fa3d6a99f0a608b6c70b3fda4fbe98dc9
pdias-felipe <pdias.felipe@gmail.com>
2022-11-25 15:15:17 -0300

    add axios/config.js

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import blogFetch from '../axios/config';
 
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
@@ -12,7 +12,7 @@ const Home = () => {
 
   const getPosts = async() => { /*responsavel por pegar os dados da API -- tipo fetch*/
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      const response = await blogFetch.get('/posts')
       const data = response.data;
       setPosts(data)
     } catch (error) {
